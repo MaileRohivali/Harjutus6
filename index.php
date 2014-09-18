6d0c9b738d52eead02f6c0f808459df5199047ed
Maile.Rohiväli <maile.rohivali@gmail.com>
2014-09-18 13:32:54 +0300

    Add if, elseif, switch

--- index.php
+++ index.php
@@ -24,5 +24,42 @@
   		echo "Kahjuks olete liiga noor. Te peate olema vähemalt {$age_limit} aastat vana. Tulge " . ($age_limit - $age) . " aasta pärast tagasi";
   	}
   	?>
+    <?php $g = 3;
+    $h = 3;
+    $i = 4;
+    $j = 5;
+
+    if (($g==$h) && ($i>$j)) {
+      echo "Kui a on väiksem kui b ning b on suurem kui c, siis käivitatakse see koodiplokk.";
+      // üks komponentlause oli tõene, kuid teine oli väär. Kood ei käivitunud, sest mõlemad pidid õiged olema - JA.
+    } 
+    echo "<br>";
+    if (($g==$h) || ($i>$j)) {
+      echo "Kui väheamlt üks komponentlausetest on tõene, siis käivitatakse see koodiplokk.";
+      //Üks komponentlause on tõene, kood käivitus
+    }
+    ?><br>
+    <?php
+    if (!isset($nothing)) {
+      echo "Kosmos kutsub!";
+    }
+    ?><br>
+
+    <h3>Switch</h3>
+    <?php $current_language = 'fi';
+    switch($current_language) {
+      case 'et':
+    echo "Tere! Kuidas sul läheb?";
+      break;
+      case 'ru':
+    echo "Здравствуйте! Как вы делаете?";
+      break;  
+      case 'fi':
+    echo "Hei! Miten menee?";
+      break;
+    }
+    ?>
+
+
   </body>
  </html>
